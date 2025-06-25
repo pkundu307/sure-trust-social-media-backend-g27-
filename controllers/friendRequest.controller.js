355bd6ec136d912a684248d66b4ef39a8e5dcde6
pkundu307 <pkundu307@gmail.com>
2025-06-25 21:55:31 +0530

    friend request related apis added

--- controllers/friendRequest.controller.js
+++ controllers/friendRequest.controller.js
@@ -30,3 +30,68 @@ export const sendFriendRequest = async (req, res) => {
         
     }
 }
+
+export const getAllFriendRequest = async (req, res) => {
+    const userId = req.user.userId;
+    if (!userId) {
+        return res.status(400).json({ message: "invalid request" });
+    }
+    try {
+        const friendRequests = await FriendRequest.find({ to: userId, status: "pending" })
+            .populate("from", "name email")
+            .sort({ createdAt: -1 });
+
+        res.status(200).json(friendRequests);
+    } catch (error) {
+        console.error("Error fetching friend requests:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const acceptFriendRequest = async (req, res) => {
+    const userId = req.user.userId;
+    
+    const requestId= req.params.id;
+    console.log(userId, requestId);
+    
+    if (!userId || !requestId) {
+        return res.status(400).json({ message: "Invalid request" });
+    }
+
+    try {
+       const request = await FriendRequest.findById(requestId);
+        if (!request || request.status !== "pending" || request.to.toString() !== userId) {
+            return res.status(404).json({ message: "Friend request not found" });
+        }
+
+
+
+
+        request.status = "accepted";
+        await request.save();
+
+        await User.findByIdAndUpdate(request.from, {
+            $addToSet: { followers: request.to } // Add userId to the followers array
+        });
+        await User.findByIdAndUpdate(request.to, {
+            $addToSet: { followers: request.from } // Add fromUserId to the followers array
+        });
+
+        res.status(200).json({ message: "Friend request accepted", request });
+    } catch (error) {
+        console.error("Error accepting friend request:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const getAllFriends = async (req, res) => {
+    try {
+      const user = await User.findById(req.user.userId).populate('followers', 'name email')
+      console.log(user);
+      
+      res.json(user.followers);
+    } catch (err) {
+      res.status(500).json({ message: 'Failed to fetch friends' });
+    }
+  };
+  
--- routes/friendRequest.routes.js
+++ routes/friendRequest.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { sendFriendRequest } from '../controllers/friendRequest.controller.js';
+import { acceptFriendRequest, getAllFriendRequest, getAllFriends, sendFriendRequest } from '../controllers/friendRequest.controller.js';
 import { protect } from '../utilities/protechjwt.js';
 
 const router = express.Router();
 
 router.post('/send', protect,sendFriendRequest);
-
+router.get('/all', protect, getAllFriendRequest);
+router.get('/allfriends',protect,getAllFriends)
+router.put('/accept/:id', protect, acceptFriendRequest);
 export default router;
