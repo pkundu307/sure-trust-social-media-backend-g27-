e8126eb8c7411bd30ec7dce0cbbf535958679ae3
pkundu307 <pkundu307@gmail.com>
2025-06-28 19:44:03 +0530

    new post api

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,5 +1,5 @@
 import { Post } from "../models/Post.js";
-
+import {User}from "../models/User.js";
 export const addPost = async (req, res) => {
   const { text, image } = req.body;
   try {
@@ -14,7 +14,7 @@ export const addPost = async (req, res) => {
   }
 };
 
-export const getAllPosts = async (req, res) => {
+export const getMyAllPosts = async (req, res) => {
   try {
     const user = req.user.userId;
     console.log(user, "0-0-0-");
@@ -22,6 +22,25 @@ export const getAllPosts = async (req, res) => {
     const posts = await Post.find({ user })
       .sort({ createdAt: -1 })
       .populate("user");
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getAllPosts = async (req, res) => {
+  try {
+    const user = req.user.userId;
+    console.log(req.user, "0-0-0-");
+    const mainUser = await User.findById(req.user.userId).select('followers')
+    const friendIds = mainUser.followers.map(id=> id.toString());
+    friendIds.push(user.toString());
+
+    const posts = await Post.find({ user:{$in:friendIds} })
+      .sort({ createdAt: -1 })
+      .populate("user",'name profilePicture')
+      .populate("comments.user", "name profilePicture");
 
     res.status(200).json(posts);
   } catch (error) {
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { addComment, addPost, getAllPosts, likePost } from '../controllers/post.controller.js';
+import { addComment, addPost, getAllPosts, getMyAllPosts, likePost } from '../controllers/post.controller.js';
 import { protect } from '../utilities/protechjwt.js';
 
 const router = express.Router();
 
 router.post('/add',protect,addPost)
 router.get('/all',protect,getAllPosts)
+router.get('/allofme',protect,getMyAllPosts)
 router.post('/like/:id',protect,likePost)
 router.post('/comment/:id',protect,addComment)
 
