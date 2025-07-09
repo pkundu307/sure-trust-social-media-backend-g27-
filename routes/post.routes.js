38beaa2f511969e4b3c99b8c848c2e1c6b8e2328
vasanthikondreddy <k.vasanthi882005@gmail.com>
2025-07-09 22:03:48 +0530

    Task-1: Delete post route implemented

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,13 +1,24 @@
 import express from 'express'
-import { addComment, addPost, getAllPosts, getMyAllPosts, likePost } from '../controllers/post.controller.js';
+import { 
+  addComment, 
+  addPost, 
+  getAllPosts, 
+  getMyAllPosts, 
+  likePost,
+  deletePost              // delete
+} from '../controllers/post.controller.js';
+
 import { protect } from '../utilities/protechjwt.js';
 
 const router = express.Router();
 
-router.post('/add',protect,addPost)
-router.get('/all',protect,getAllPosts)
-router.get('/allofme',protect,getMyAllPosts)
-router.post('/like/:id',protect,likePost)
-router.post('/comment/:id',protect,addComment)
+router.post('/add', protect, addPost);
+router.get('/all', protect, getAllPosts);
+router.get('/allofme', protect, getMyAllPosts);
+router.post('/like/:id', protect, likePost);
+router.post('/comment/:id', protect, addComment);
+
+// task 1 route added
+router.delete('/post/:id', protect, deletePost);
 
 export default router;
